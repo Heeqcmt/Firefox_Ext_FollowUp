c2b63b25520148f0c4c7d59a497d4317e4d3d5e3
YuMing Zhang <yumingzhang94@gmail.com>
2021-04-14 14:38:12 -0400

    added notifications

--- followUp.js
+++ followUp.js
@@ -1,10 +1,16 @@
-
+/*
+bookmarkID key: tabID value: bookmarkID
+lastOpenURL key: tabID value: last open URL
+*/
 let bookmarkID = new Map();
 let lastOpenURL=new Map();
 //check if url is in the bookmark
 function checkBookmark(tabID,changeInfo,tabInfo){
 
     console.log(tabInfo.url);
+    //check if url of tabID is in the bookmark tree.
+    //if url is one of the bookmark, save bookmark id and url along with tabid as keys
+    //save the url in every onUpdate calls
     let searching = browser.bookmarks.search({url: tabInfo.url});
     searching.then(
         bookmarkItem =>{
@@ -27,11 +33,28 @@ function checkBookmark(tabID,changeInfo,tabInfo){
 }
 
 //update bookmark url when tab is closed
+//check if tab once contains bookmarked content
+//if so update the bookmark with current url
+//new feature: adding user comfirmation before the update via a notification
 
 function updateBookmark(tabID, removedInfo){
+
+    
+
+    let preUpdateNotification;
+
+
     if(lastOpenURL.has(tabID)){
         browser.bookmarks.update(bookmarkID.get(tabID),{url:lastOpenURL.get(tabID)}).then(
             ()=>{console.log(`${lastOpenURL.get(tabID)}`)},()=>{console.log("error")}
+        )
+        browser.notifications.create(
+            preUpdateNotification,
+            {
+                "type":"basic",
+                "message":`bookmark changed to ${lastOpenURL.get(tabID)}`,
+                "title":"Follow Up detected a possible bookmark update",
+            }
         )
     }
 }
